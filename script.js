1b3ea9045c5d6eb634a124e120ff8eff38ad72fd
Yordan Angelov <32510422+YordanAngelov@users.noreply.github.com>
2020-09-02 17:15:58 +0100

    basic intro

--- script.js
+++ script.js
@@ -4,9 +4,28 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+const main = document.getElementById("root");
+const episodeCard = document.querySelector(".card");
+const episodeTitle = document.getElementById("title");
+const episodeImg = document.getElementById("image");
+const episodeSum = document.getElementById("summary");
+const seasonNum = document.getElementById("seriNum");
+const episodeNum = document.getElementById("epNum");
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  episodeList.forEach(function (episode) {
+    episodeTitle.textContent = episode.name;
+    seasonNum.textContent = episode.season;
+    episodeNum.textContent = episode.number;
+    episodeImg.setAttribute("src", episode.image.medium);
+    episodeSum.textContent = episode.summary;
+  });
+
+//function makePageForEpisodes(episodeList) {
+//  const rootElem = document.getElementById("root");
+//  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+//}
 
 window.onload = setup;
